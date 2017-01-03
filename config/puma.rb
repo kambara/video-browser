025b9f6e959bc50f5355dc79d8aaa2510c3e69a9
Kambara Keisuke <kambara@sappari.org>
2017-01-03 18:40:14 +0900

    delete .gitkeep

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,8 @@
 root = "#{Dir.getwd}"
 
-bind "unix://#{root}/tmp/socket"
-pidfile "#{root}/tmp/pid"
-state_path "#{root}/tmp/state"
+bind "unix://#{root}/tmp/puma/socket"
+pidfile "#{root}/tmp/puma/pid"
+state_path "#{root}/tmp/puma/state"
 rackup "#{root}/config.ru"
 
 threads 4, 8
