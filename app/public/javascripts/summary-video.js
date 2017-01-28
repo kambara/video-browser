94eee58045f2cff9b1b0a94143553dbfac8eec89
Kambara Keisuke <kambara@sappari.org>
2017-01-28 10:50:50 +0900

    Remove semiclons in javascripts

--- app/public/javascripts/summary-video.js
+++ app/public/javascripts/summary-video.js
@@ -1,56 +1,56 @@
 class SummaryVideo {
   constructor() {
-    this.offset = 30;
-    this.durationPerScene = 5;
-    this.numberOfScenes = 6;
-    this.currentIndex = 0;
+    this.offset = 30
+    this.durationPerScene = 5
+    this.numberOfScenes = 6
+    this.currentIndex = 0
     // Video
-    this.video = $('#summary');
-    this.videoElement = this.video.get(0);
+    this.video = $('#summary')
+    this.videoElement = this.video.get(0)
     this.video.one('canplay', () => {
       const duration = this.videoElement.duration
-      this.interval = (duration - this.offset) / this.numberOfScenes;
-      this.play();
-    });
-    this.video.on('timeupdate', () => { this.onTimeUpdate(); });
-    this.video.on('click', () => { this.onClick(); });
+      this.interval = (duration - this.offset) / this.numberOfScenes
+      this.play()
+    })
+    this.video.on('timeupdate', () => { this.onTimeUpdate() })
+    this.video.on('click', () => { this.onClick() })
     this.videoElement.volume = 0
   }
 
   play() {
-    this.videoElement.currentTime = this.getStartTime();
-    this.videoElement.play();
+    this.videoElement.currentTime = this.getStartTime()
+    this.videoElement.play()
   }
 
   onTimeUpdate() {
     if (this.videoElement.currentTime
         > this.getStartTime() + this.durationPerScene) {
-      this.videoElement.pause();
-      this.video.css('visibility', 'hidden');
-      this.currentIndex++;
+      this.videoElement.pause()
+      this.video.css('visibility', 'hidden')
+      this.currentIndex++
       if (this.currentIndex > this.numberOfScenes - 1) {
-        this.currentIndex = 0;
+        this.currentIndex = 0
       }
       setTimeout(() => {
-        this.play();
+        this.play()
         this.video.one('canplay', () => {
-          this.video.css('visibility', 'visible');
-        });
-      }, 200);
+          this.video.css('visibility', 'visible')
+        })
+      }, 200)
     }
   }
 
   onClick() {
     if (this.videoElement.paused) {
-      this.videoElement.play();
+      this.videoElement.play()
     } else {
-      this.videoElement.pause();
+      this.videoElement.pause()
     }
   }
 
   getStartTime() {
-    return this.offset + this.interval * this.currentIndex;
+    return this.offset + this.interval * this.currentIndex
   }
 }
 
-new SummaryVideo();
+new SummaryVideo()
--- app/public/javascripts/video.js
+++ app/public/javascripts/video.js
@@ -1,31 +1,31 @@
 function main(videoFileUrl) {
-  VideoFactory.create(videoFileUrl);
+  VideoFactory.create(videoFileUrl)
 }
 
 class VideoFactory {
   static create(videoFileUrl) {
     if (videoFileUrl.match(/\.mp4\.link$/)) {
-      return new Video(videoFileUrl, '.video-container');
+      return new Video(videoFileUrl, '.video-container')
     } else if (!this.isChrome()) {
-      return new VlcVideo(videoFileUrl, '.video-container');
+      return new VlcVideo(videoFileUrl, '.video-container')
     }
   }
 
   static isChrome() {
-    var ua = window.navigator.userAgent.toLowerCase();
+    var ua = window.navigator.userAgent.toLowerCase()
     (ua.indexOf('chrome') >= 0)
   }
 }
 
 class Video {
   constructor(videoFileUrl, container) {
-    this.video = this.createElement(videoFileUrl);
-    this.videoElement = this.video.get(0);
-    $(container).append(this.video);
+    this.video = this.createElement(videoFileUrl)
+    this.videoElement = this.video.get(0)
+    $(container).append(this.video)
     $(window).keydown((event) => {
-      this.onKeyDown(event);
-    });
-    this.initVideo();
+      this.onKeyDown(event)
+    })
+    this.initVideo()
   }
 
   createElement(url) {
@@ -33,116 +33,116 @@ class Video {
       controls: 'controls',
       preload: 'auto',
       src: url
-    });
-    return video;
+    })
+    return video
   }
 
   initVideo() {
-    this.video.css('visibility', 'hidden');
+    this.video.css('visibility', 'hidden')
     this.video.one('canplay', () => {
-      this.seekAndPlay(this.getInitialTime());
-    });
+      this.seekAndPlay(this.getInitialTime())
+    })
   }
 
   onKeyDown(event) {
-    // console.log(event.keyCode);
+    // console.log(event.keyCode)
     switch (event.keyCode) {
       case 37: // Left
-        this.seekBackward(10);
-        break;
+        this.seekBackward(10)
+        break
       case 39: // Right
-        this.seekForward(10);
-        break;
+        this.seekForward(10)
+        break
       case 38: // top
-        this.seekForward(60);
-        break;
+        this.seekForward(60)
+        break
       case 40: // Bottom
-        this.seekBackward(60);
-        break;
+        this.seekBackward(60)
+        break
       case 32: // Space
-        this.onSpaceKeyDown();
-        break;
+        this.onSpaceKeyDown()
+        break
       case 70: // F
-        this.toggleFullscreen();
-        break;
+        this.toggleFullscreen()
+        break
     }
   }
 
   onSpaceKeyDown() {
-    this.togglePause();
+    this.togglePause()
   }
 
   togglePause() {
     if (this.videoElement.paused) {
-      this.videoElement.play();
+      this.videoElement.play()
     } else {
-      this.videoElement.pause();
+      this.videoElement.pause()
     }
   }
 
   toggleFullscreen() {
     if (document.webkitIsFullScreen) {
-      this.videoElement.webkitExitFullscreen();
+      this.videoElement.webkitExitFullscreen()
     } else {
-      this.videoElement.webkitRequestFullscreen();
+      this.videoElement.webkitRequestFullscreen()
     }
   }
 
   seekAndPlay(time) {
     this.video.one('seeked', () => {
-      this.video.css('visibility', 'visible');
-      this.videoElement.play();
-    });
-    this.videoElement.currentTime = time;
+      this.video.css('visibility', 'visible')
+      this.videoElement.play()
+    })
+    this.videoElement.currentTime = time
   }
 
   getCurrentTime() {
-    return this.videoElement.currentTime;
+    return this.videoElement.currentTime
   }
 
   seekForward(time) {
-    this.seekAndPlay(this.getCurrentTime() + time);
-    this.showControls();
+    this.seekAndPlay(this.getCurrentTime() + time)
+    this.showControls()
   }
 
   seekBackward(time) {
-    this.seekAndPlay(this.getCurrentTime() - time);
-    this.showControls();
+    this.seekAndPlay(this.getCurrentTime() - time)
+    this.showControls()
   }
 
   showControls() {
-    this.videoElement.controls = true;
+    this.videoElement.controls = true
   }
 
   getInitialTime() {
     if (location.hash.length <= 1) {
-      return 0;
+      return 0
     }
-    var pair = location.hash.substr(1).split('=');
+    var pair = location.hash.substr(1).split('=')
     if (pair[0] == 'time') {
-      var time = parseInt(pair[1]);
+      var time = parseInt(pair[1])
       if (!isNaN(time)) {
-        return time;
+        return time
       }
     }
-    return 0;
+    return 0
   }
 }
 
 class VlcVideo extends Video {
   constructor(videoFileUrl, container) {
-    super(videoFileUrl, container);
+    super(videoFileUrl, container)
   }
 
   initVideo() {
     // Reload if plugin error happens
     if (!this.videoElement.input) {
       setTimeout(() => {
-        location.reload();
-      }, 1000);
-      return;
+        location.reload()
+      }, 1000)
+      return
     }
-    this.seekAndPlay(this.getInitialTime());
+    this.seekAndPlay(this.getInitialTime())
   }
 
   createElement(url) {
@@ -155,12 +155,12 @@ class VlcVideo extends Video {
       branding: false,
       width: 0,
       height: 0
-    });
-    return video;
+    })
+    return video
   }
 
   getCurrentTime() {
-    return this.videoElement.input.time / 1000;
+    return this.videoElement.input.time / 1000
   }
 
   showControls() {
@@ -170,14 +170,14 @@ class VlcVideo extends Video {
   }
 
   toggleFullscreen() {
-    this.videoElement.video.toggleFullscreen();
-    this.videoElement.focus();
+    this.videoElement.video.toggleFullscreen()
+    this.videoElement.focus()
   }
 
   seekAndPlay(time) {
     if (this.videoElement.input.state != 4) {
-      this.videoElement.playlist.play();
+      this.videoElement.playlist.play()
     }
-    this.videoElement.input.time = time * 1000;
+    this.videoElement.input.time = time * 1000
   }
 }
